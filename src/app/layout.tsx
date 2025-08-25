f82842b0561a67784cb70008a89f2f1e8b68a5e0
Bruno Barretta <bruno.barretta@fatec.sp.gov.br>
2025-08-25 20:13:57 -0300

    feat: Update fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import localFont from "next/font/local";
 import "../styles/globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 
 const gistesy = localFont({
   src: "../assets/fonts/Gistesy.ttf",
@@ -27,16 +15,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="pt-BR">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${gistesy.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="pt-BR" className={gistesy.variable}>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
